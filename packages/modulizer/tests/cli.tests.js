a323c5a2a1dec34931e8a1392b4c07648770a30f
Ian Walter <pub@ianwalter.dev>
2020-12-27 23:42:48 -0500

    Working tests

--- packages/modulizer/tests/cli.tests.js
+++ packages/modulizer/tests/cli.tests.js
@@ -2,30 +2,30 @@ import { readFileSync } from 'fs'
 import { test } from '@ianwalter/bff'
 import execa from 'execa'
 
-test('cjs file not generated when --esm specified', async ({ expect }) => {
+test('cjs file not generated when --esm specified', async t => {
   const input = 'tests/fixtures/exportDefaultFunction.js'
   const esm = 'tmp/one.js'
   const args = [input, '--esm', esm]
   const { stdout } = await execa('./cli.js', args)
-  expect(stdout).toMatchSnapshot()
-  expect(readFileSync(esm, 'utf8')).toMatchSnapshot()
+  t.expect(stdout).toMatchSnapshot()
+  t.expect(readFileSync(esm, 'utf8')).toMatchSnapshot()
 })
 
-test('dist file is transpiled when --babel passed', async ({ expect }) => {
+test('dist file is transpiled when --babel passed', async t => {
   const input = 'tests/fixtures/exportDefaultNewExpression.js'
   const cjs = 'tmp/two.js'
   const args = [input, '--cjs', cjs, '--babel']
   const { stdout } = await execa('./cli.js', args)
-  expect(stdout).toMatchSnapshot()
-  expect(readFileSync(cjs, 'utf8')).toMatchSnapshot()
+  t.expect(stdout).toMatchSnapshot()
+  t.expect(readFileSync(cjs, 'utf8')).toMatchSnapshot()
 })
 
-test('dist file generated using custom plugins', async ({ expect }) => {
+test('dist file generated using custom plugins', async t => {
   const input = 'tests/fixtures/exportObjectWithVueComponent.js'
   const cjs = 'tmp/three.js'
   const plugins = ['--plugins', 'tests/helpers/vuePlugin.js']
   const args = [input, '--cjs', cjs, ...plugins]
   const { stdout } = await execa('./cli.js', args)
-  expect(stdout).toMatchSnapshot()
-  expect(readFileSync(cjs, 'utf8')).toMatchSnapshot()
+  t.expect(stdout).toMatchSnapshot()
+  t.expect(readFileSync(cjs, 'utf8')).toMatchSnapshot()
 })
--- packages/modulizer/tests/dist.tests.js
+++ packages/modulizer/tests/dist.tests.js
@@ -5,43 +5,43 @@ import modulize from '../index.js'
 const output = '/fakePath'
 const dirname = path.dirname(import.meta.url.replace('file://', ''))
 
-test('export default literal converted to module.exports', async ctx => {
+test('export default literal converted to module.exports', async t => {
   const name = 'exportDefaultLiteral'
   const input = path.join(dirname, `fixtures/${name}.js`)
   const cjs = path.join(output, `${name}.js`)
   const options = { name, input, output, cjs, browser: true }
-  ctx.expect(await modulize(options)).toMatchSnapshot()
+  t.expect(await modulize(options)).toMatchSnapshot()
 })
 
-test('export default function converted to module.exports', async ctx => {
+test('export default function converted to module.exports', async t => {
   const input = path.join(dirname, 'fixtures/exportDefaultFunction.js')
   const cjs = path.join(output, 'some-function.js')
-  ctx.expect(await modulize({ input, output, cjs })).toMatchSnapshot()
+  t.expect(await modulize({ input, output, cjs })).toMatchSnapshot()
 })
 
-test('export default new expression converted to module.exports', async ctx => {
+test('export default new expression converted to module.exports', async t => {
   const input = path.join(dirname, 'fixtures/exportDefaultNewExpression.js')
-  ctx.expect(await modulize({ input, output, cjs: true })).toMatchSnapshot()
+  t.expect(await modulize({ input, output, cjs: true })).toMatchSnapshot()
 })
 
-test('all imports get bundled with module into pack files', async ctx => {
+test('all imports get bundled with module into dist files', async t => {
   const name = 'exportDefaultFunctionWithImports'
   const input = path.join(dirname, `fixtures/${name}.js`)
   const cjs = path.join(output, `${name}.js`)
   const options = { name, input, output, cjs, esm: true, inline: '' }
-  ctx.expect(await modulize(options)).toMatchSnapshot()
+  t.expect(await modulize(options)).toMatchSnapshot()
 })
 
-test('specified import gets bundled with module into pack file', async ctx => {
+test('specified import gets bundled with module into dist file', async t => {
   const input = path.join(dirname, 'fixtures/exportObjectWithImports.js')
   const cjs = path.join(output, 'exportObjectWithImports.js')
   const inline = '@ianwalter/npm-short-name'
   const options = { input, output, cjs, inline, babel: true }
-  ctx.expect(await modulize(options)).toMatchSnapshot()
+  t.expect(await modulize(options)).toMatchSnapshot()
 })
 
-test.skip('hashbang is preserved', async ctx => {
+test.skip('hashbang is preserved', async t => {
   const input = path.join(dirname, 'fixtures/cli.js')
   const cjs = path.join(output, 'cli.js')
-  ctx.expect(await modulize({ input, output, cjs })).toMatchSnapshot()
+  t.expect(await modulize({ input, output, cjs })).toMatchSnapshot()
 })
