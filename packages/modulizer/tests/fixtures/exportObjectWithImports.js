dcd4df4f40f04f9a7a474af61aa8506a6b43ea77
Ian Walter <pub@ianwalter.dev>
2021-10-12 09:07:41 -0400

    Removing p-settle

--- packages/modulizer/tests/fixtures/exportObjectWithImports.js
+++ packages/modulizer/tests/fixtures/exportObjectWithImports.js
@@ -1,9 +1,7 @@
 import npmShortName from '@ianwalter/npm-short-name'
-import pSettle from 'p-settle'
 import greeting from './exportDefaultFunction'
 
 export {
   npmShortName,
-  pSettle,
   greeting
 }
