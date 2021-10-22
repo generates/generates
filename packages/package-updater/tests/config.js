b2677042bbbca3a782dd542bee517502341eb699
Ian Walter <pub@ianwalter.dev>
2021-10-21 21:36:26 -0400

    Fixing test plugin

--- packages/package-updater/tests/config.js
+++ packages/package-updater/tests/config.js
@@ -1,8 +1,9 @@
 import execa from 'execa'
 
-export default {
-  async beforeEach () {
+export default function (plug) {
+  plug.in('beforeTest', async function createTempPackageJson (ctx, next) {
     await execa('mkdir', ['-p', 'tests/tmp'])
     await execa('cp', ['tests/fixtures/package.json', 'tests/tmp'])
-  }
+    return next()
+  })
 }
